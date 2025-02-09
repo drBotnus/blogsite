defc2213d8335e412c6d620ba37ede83ada46aa2
botnus <mikkel.beverfjord@mhbtech.dev>
2025-02-09 21:38:10 +0100

    im braindead

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let router = Router::new().route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+    println!("->> axum listening on http://{}", listener.local_addr()?);
+
+    axum::serve(listener, router).await
+}
+
+async fn index() -> impl IntoResponse {
+    (StatusCode::OK, "Homepage")
 }
